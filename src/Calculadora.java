8afea18cba57ac9bb344ba860fd801a76d978142
Daniel-Cazita <cazita4senai@gmail.com>
2025-08-28 14:47:59 -0300

    Commit - 2
    Adicionando Divisão e Multiplicação

---
+++ src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public static int multiplicacao(int a, int b) {
+        return a * b;
+    } 
+
+    public static int divisao(int a, int b) {
+        return a / b;
+    } 
+}
